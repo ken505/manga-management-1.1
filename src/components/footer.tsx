afaa17fb9e63bf9ed0baff35955d9d3a3bb27348
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-12 17:56:39 +0900

    ✨ Initial setup

---
+++ src/components/footer.tsx
@@ -0,0 +1,6 @@
+export const Footer = () => {
+  return (
+    <footer className="px-4 text-gray-600 bg-gray-200">
+      <small>&copy; 2021 ken.plus</small>
+</footer> );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,49 @@
+import { Auth, Button, IconLogOut } from "@supabase/ui";
+import type { ReactNode } from "react";
+import React from "react";
+import { LayoutWrapper } from "src/components/layoutWrapper";
+import { client } from "src/libs/supabase";
+type Props = {
+  children: ReactNode;
+};
+const Container = (props: Props) => {
+  const { user } = Auth.useUser();
+  // ログインしている場合
+  if (user) {
+    return (
+      <div>
+        <div className="flex justify-end mx-2 my-4">
+          <Button
+            size="medium"
+            icon={<IconLogOut />}
+            onClick={() => client.auth.signOut()}
+          >
+            Sign out
+          </Button>
+        </div>
+      </div>
+    );
+  }
+  // ログインしていない場合
+  return <>{props.children}</>;
+};
 const Home = () => {
   return (
-    <div>
-    
-    </div>
+    <LayoutWrapper>
+      <Auth.UserContextProvider supabaseClient={client}>
+        <Container>
+          <div className="flex justify-center pt-8">
+            <div className="w-full sm:w-96">
+              <Auth
+                supabaseClient={client}
+                providers={["github"]}
+                socialColors={true}
+              />
+            </div>
+          </div>
+        </Container>
+      </Auth.UserContextProvider>
+    </LayoutWrapper>
   );
 };
 export default Home;
